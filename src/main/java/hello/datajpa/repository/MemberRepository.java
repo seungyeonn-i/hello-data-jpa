d9514a412ceb96840aa9870a7bfc23a45de10ef0
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-27 03:25:17 +0900

    ☑️ Spring Data Jpa , H2 동작 확인및 예제 도메인 동작 확인

---
+++ src/main/java/hello/datajpa/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package hello.datajpa.repository;
+
+import hello.datajpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
---
+++ src/test/java/hello/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package hello.datajpa.repository;
+
+import hello.datajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장. 같은 인스턴스임
+    }
+
+}
