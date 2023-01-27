2b31d4200b5cd824ae1d6b72fe07672efbb56a2c
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-01-28 03:04:19 +0900

    ☑️ 순수 JPA 기반 레포지토리

---
+++ src/main/java/hello/datajpa/repository/TeamRepository.java
@@ -0,0 +1,44 @@
+package hello.datajpa.repository;
+
+import hello.datajpa.entity.Member;
+import hello.datajpa.entity.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll(){
+        return em.createQuery("select t FROM Team t", Team.class)
+                .getResultList();
+    }
+
+    public Optional<Team> findByID(Long id){
+        Team Team = em.find(Team.class, id);
+        return Optional.ofNullable(Team);
+    }
+
+    public long count() {
+        return em.createQuery("select count(t) from Team t", Long.class)
+                .getSingleResult();
+    }
+    public Team find(Long id) {
+        return em.find(Team.class, id);
+    }
+}
--- src/test/java/hello/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/hello/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +30,34 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장. 같은 인스턴스임
+    }
+
+    @Test
+    public void baseCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findByID(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findByID(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member11111");
+
+//        List<Member> all = memberJpaRepository.findAll();
+//        assertThat(all.size()).isEqualTo(2);
+//
+//        long count = memberJpaRepository.count();
+//        assertThat(count).isEqualTo(2);
+//
+//        memberJpaRepository.delete(member1);
+//        memberJpaRepository.delete(member2);
+//
+//        long deletecount = memberJpaRepository.count();
+//        assertThat(deletecount).isEqualTo(0);
     }
 
 }
